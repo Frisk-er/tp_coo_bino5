094ec00d1a702aba4c3d04d1718289e927933374
Amine Lalouche <llm2705a@3a-g26-12d.univ-tlse3.fr>
2023-10-06 17:34:40 +0200

    test.py

--- brewsim/high_level/tests.py
+++ brewsim/high_level/tests.py
@@ -2,12 +2,37 @@
 
 from django.test import TestCase
 
-from .models import Machine
+from . import models
+
+# class MachineModelTests(TestCase):
+#   def test_usine_creation(self):
+#      self.assertEqual(models.Machine.objects.count(), 0)
+#     models.Machine.objects.create(nom="four", prix=1_000)
+#
+#    self.assertEqual(Machine.objects.count(), 1)
 
 
-class MachineModelTests(TestCase):
-    def test_usine_creation(self):
-        self.assertEqual(Machine.objects.count(), 0)
-        Machine.objects.create(nom="four", prix=1_000)
+class ModelTestsCosts(TestCase):
+    def test_costs(self):
+        ing1 = models.Ingredient.objects.create(nom="houblon")
+        ing2 = models.Ingredient.objects.create(nom="orge")
+        sto1 = models.QuantiteIngredient.objects.create(ingredient=ing1, quantite=50)
+        sto2 = models.QuantiteIngredient.objects.create(ingredient=ing2, quantite=100)
+        dep = models.Departement.objects.create(numero=31, prixM2=2000)
+        Mach1 = models.Machine.objects.create(nom="Mach1", prix=1000)
+        Mach2 = models.Machine.objects.create(nom="Mach1", prix=2000)
+        Ac1 = models.Action.objects.create(
+            machine=Mach1, commandes="malaxer", duree=5, ingredient=ing1
+        )
+        Rec = models.Recette.objects.create(nom="blonde", action=Ac1)
 
-        self.assertEqual(Machine.objects.count(), 1)
+        usi = models.Usine.objects.create()
+        usi.departement.add(dep)
+        usi.taille.add(50)
+        usi.machines.add(Mach1)
+        usi.machines.add(Mach2)
+        usi.recettes.add(Rec)
+        usi.stocks.add(sto1)
+        usi.stocks.add(sto2)
+
+        usi.objects.costs()
