9532b06a24725900498b10f0be64d318e3358ec0
amine <aminelalouche@yahoo.fr>
2024-01-02 12:32:14 +0100

    suppression des prints de bebugage

--- brewsim/low_level/low_level.cpp
+++ brewsim/low_level/low_level.cpp
@@ -139,19 +139,12 @@ class Action{
     	cpr::Response r = cpr::Get(cpr::Url{u});
     	std::cout << r.status_code << std::endl; // 200
     	json j=json::parse(r.text);
-    	std::cout<<"action1"<<std::endl;
     	machine_=std::make_unique<Machine>(j["machine"]);
-    	std::cout<<"action2"<<std::endl;
     	commandes_=j["commandes"];
-    	std::cout<<"action2.1"<<std::endl;
     	duree_=j["duree"];
-    	std::cout<<"action3"<<std::endl;
     	ingredient_=std::make_unique<Ingredient>(j["ingredient"]);
-    	std::cout<<"action4"<<std::endl;
     	if(j.contains("action") )
     	action_=std::make_unique<Action>(j["action"]);
-
-	std::cout<<"action5"<<std::endl;
 	}
 
 	friend std::ostream& operator<<(std::ostream& out, const Action& p) {
@@ -191,30 +184,25 @@ class Usine{
 	Usine(int id){
 	std::string u="http://localhost:8000/Usine/"+std::to_string(id);
     	cpr::Response r = cpr::Get(cpr::Url{u});
-
-    	std::cout<<"balise 1"<<std::endl;
     	std::cout << r.status_code << std::endl; // 200
     	json j=json::parse(r.text);
-    	std::cout<<"balise 2"<<std::endl;
+
     	departement_=std::make_unique<Departement>(j["departement"]);
-    	std::cout<<"balise 3"<<std::endl;
     	taille_=j["taille"];
-    	std::cout<<"balise 4"<<std::endl;
     	machines_= std::vector<std::unique_ptr<Machine>>();
     	for(const auto &Mach:j["machines"]){
     		machines_.push_back(std::make_unique<Machine>(Mach));
     	}
-    	std::cout<<"balise 5"<<std::endl;
+
 	recettes_= std::vector<std::unique_ptr<Recette>>();
 	for(const auto &Rec:j["recettes"]){
     		recettes_.push_back(std::make_unique<Recette>(Rec));
     	}
-    	std::cout<<"balise 6"<<std::endl;
+
 	stocks_= std::vector<std::unique_ptr<QuantiteIngredient>>();
 	for(const auto &Sto:j["stocks"]){
     		stocks_.push_back(std::make_unique<QuantiteIngredient>(Sto));
     	}
-	std::cout<<"balise 7"<<std::endl;
 	}
 
 	friend std::ostream& operator<<(std::ostream& out, const Usine& p) {
@@ -274,20 +262,15 @@ int main() {
     Machine mach2=(2);
     std::cout<<mach2<<std::endl;
 
-    std::cout<<"main1"<<std::endl;
 
     Action act1=(1);
     std::cout<<act1<<std::endl;
 
-    std::cout<<"main2"<<std::endl;
-
     Recette rec1=(1);
     std::cout<<rec1<<std::endl;
 
-    std::cout<<"main3"<<std::endl;
-
-    Usine usibis=(i);
-    std::cout<<usi<<std::endl;
+    //Usine usibis=(i);
+    //std::cout<<usi<<std::endl;
 
     std::cout<<"main_fin"<<std::endl;
 
